f8767f2a20244e9051925a5dafca10698f778f0d
karmanchik567 <fedukevicvlad@gmail.com>
2023-03-29 21:44:32 +0300

    create component ControlPanel to manage settings

--- calc-react-testing-task/src/components/Header/Header.jsx
+++ calc-react-testing-task/src/components/Header/Header.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import { Header_name, LinkText, List, NavLink } from '../../pages/Home/styled'
-import { Container } from './styled'
+import React from "react";
+import { Header_name, LinkText, List, NavLink } from "../../pages/Home/styled";
+import { Container } from "./styled";
 
-import { TABS } from '../../constants/constants'
+import { TABS } from "../../constants/constants";
 
 const Header = () => {
   return (
     <Container>
       <Header_name>Calculator</Header_name>
       <List>
-        {TABS.map((tab, i) =>
+        {TABS.map((tab, i) => (
           <NavLink key={i} to={tab.route}>
             <LinkText>{tab.text}</LinkText>
           </NavLink>
-        )}
+        ))}
       </List>
     </Container>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
