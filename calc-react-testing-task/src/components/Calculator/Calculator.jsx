1605e27f799cfe7e49cd0a885a75bd137c7de38e
karmanchik567 <fedukevicvlad@gmail.com>
2023-04-18 20:18:59 +0300

    trea

--- calc-react-testing-task/src/components/Calculator/Calculator.jsx
+++ calc-react-testing-task/src/components/Calculator/Calculator.jsx
@@ -11,37 +11,86 @@ import { setOutputDisplay, setHistory } from "@utils/actions";
 import { Wrapper } from "./styled";
 
 const mapStateToProps = (state) => ({
-  outputDisplay: state.outputDisplay,
-  history: state.history,
+    outputDisplay: state.outputDisplay,
+    history: state.history,
 });
 
 const mapDispatchToProps = {
-  setOutputDisplay,
-  setHistory,
+    setOutputDisplay,
+    setHistory,
 };
 
-const Calculator = ({ outputDisplay, setOutputDisplay, history, setHistory }) => {
-  const handleCalculate = () => {
-    const result = eval(outputDisplay.join(""));
-    setHistory([...history, `${outputDisplay.join("")}=${result}`]);
-    setOutputDisplay([result.toString()]);
-  };
+const Calculator = ({
+    outputDisplay,
+    setOutputDisplay,
+    history,
+    setHistory,
+}) => {
+    const indexOperator = (expression) => {
+        for (let i = 0; i < expression.length; i++) {
+            if (["*", "/", "+", "-"].includes(expression[i])) return i;
+        }
+        return -1;
+    }
 
-  useEffect(() => {
-    console.log(outputDisplay);
-  }, [outputDisplay]);
+    const buildExpressionTree = (expression) => {
+        const index = indexOperator(expression);
 
-  return (
-    <Wrapper>
-      <Display outputDisplay={outputDisplay} />
-      <Keypad
-        outputDisplay={outputDisplay}
-        setOutputDisplay={setOutputDisplay}
-        onCalculate={handleCalculate}
-      />
-      <History history={history} />
-    </Wrapper>
-  );
+        if (index < 0) return { value: expression };
+        else {
+            const operator = expression[index];
+            return {
+                value: operator,
+                left: buildExpressionTree(expression.slice(0, index).trim()),
+                right: buildExpressionTree(expression.slice(index + 1).trim()),
+            };
+        }
+    }
+
+    const evaluateExpression = (node) => {
+        if (!node) return 0;
+
+        if (!isNaN(node.value)) return parseFloat(node.value);
+
+        let leftValue = evaluateExpression(node.left);
+        let rightValue = evaluateExpression(node.right);
+
+        switch (node.value) {
+            case "+":
+                return leftValue + rightValue;
+            case "-":
+                return leftValue - rightValue;
+            case "*":
+                return leftValue * rightValue;
+            case "/":
+                return leftValue / rightValue;
+            default:
+                return 0;
+        }
+    }
+
+    const handleCalculate = () => {
+        const tree = buildExpressionTree(outputDisplay.join(""));
+        const result = evaluateExpression(tree)
+        setHistory([...history, `${outputDisplay.join("")}=${result}`]);
+        setOutputDisplay([result.toString()]);
+    };
+
+    useEffect(() => {
+        console.log(outputDisplay);
+    }, [outputDisplay]);
+
+    return (
+        <Wrapper>
+            <Display outputDisplay={outputDisplay} />
+            <Keypad
+                outputDisplay={outputDisplay}
+                setOutputDisplay={setOutputDisplay}
+                onCalculate={handleCalculate}
+            />
+            <History history={history} />
+        </Wrapper>
+    );
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Calculator);
