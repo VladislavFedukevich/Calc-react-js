5c95d408d7121b2a2cbfd04dd7014b577679cda8
karmanchik567 <fedukevicvlad@gmail.com>
2023-04-18 22:11:48 +0300

    fix propTypes

--- calc-react-testing-task/src/pages/Settings/Settings.jsx
+++ calc-react-testing-task/src/pages/Settings/Settings.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+
+import PropTypes from "prop-types";
 import ControlPanel from "@components/ControlPanel/ControlPanel";
 import Header from "@components/Header/Header";
+
+import { TABS } from "@constants/tabs";
+
 import { Wrapper } from "./styled";
 
 const Settings = ({ toggleTheme }) => {
     return (
         <Wrapper>
-            <Header />
+            <Header TABS={TABS} />
             <ControlPanel toggleTheme={toggleTheme} />
         </Wrapper>
     );
+};
+
+Settings.propTypes = {
+    toggleTheme: PropTypes.func.isRequired,
 };
 
 export default Settings;
